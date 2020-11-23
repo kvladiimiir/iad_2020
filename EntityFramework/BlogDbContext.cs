40c5ccebf11c5146300fb6b5ff0eb077fdff7d04
Vladimir <kozyrev-vova@mail.ru>
2020-11-23 23:21:26 +0300

    изменения

---
+++ EntityFramework/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=BlogProject;Trusted_Connection=True;");
+        }
+    }
+}
